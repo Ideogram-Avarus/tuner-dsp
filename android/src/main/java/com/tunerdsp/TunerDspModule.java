3efc40d94d418c9ba0ed579c95c57ec151e9e054
Estevao <estevao.profissionalc@gmail.com>
2026-03-05 16:03:33 -0300

    test

--- android/src/main/java/com/tunerdsp/TunerDspModule.java
+++ android/src/main/java/com/tunerdsp/TunerDspModule.java
@@ -3,14 +3,11 @@ package com.tunerdsp;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.facebook.yoga.annotations.DoNotStrip;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.WritableArray;
-import com.facebook.react.bridge.Arguments;
 import com.facebook.react.turbomodule.core.interfaces.TurboModule;
-
 import androidx.annotation.NonNull;
 
 public class TunerDspModule extends NativeTunerDspSpec implements TurboModule {
@@ -53,41 +50,25 @@ public class TunerDspModule extends NativeTunerDspSpec implements TurboModule {
     }
 
     @Override
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    public WritableArray getLatestResult() {
+        return cxxGetLatestResult();
+    }
+
+    @Override
     @ReactMethod
-    @DoNotStrip
-    public void processFrame(ReadableArray buffer) {
-        if (buffer == null) return;
-        float[] samples = new float[buffer.size()];
-        for (int i = 0; i < buffer.size(); i++) {
-            samples[i] = (float) buffer.getDouble(i);  // JS numbers → double → float
-        }
+    public void processFrame(float[] samples) {
         cxxProcessFrame(samples);
     }
 
-    
-    @Override
-    @ReactMethod(isBlockingSynchronousMethod = true)
-    @DoNotStrip
-    public WritableArray getLatestResult() {
-        double[] raw = cxxGetLatestResult();
-
-        WritableArray result = Arguments.createArray();
-        for (double val : raw) {
-            result.pushDouble(val);
-        }
-        return result;
-    }
-
     @Override
     @ReactMethod
-    @DoNotStrip
     public void reset() {
         cxxReset();
     }
     
     @Override
     @ReactMethod
-    @DoNotStrip
     public void destroy() {
         destroyEngine();
         engineCreated = false;
--- react-native.config.js
+++ react-native.config.js
@@ -2,7 +2,7 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
-        cmakeListsPath: './android/CMakeLists.txt', 
+        cmakeListsPath: './CMakeLists.txt', 
         packageImportPath: 'import com.tunerdsp.TunerDspModulePackage;',
         packageInstance: 'new TunerDspModulePackage()',
       },
